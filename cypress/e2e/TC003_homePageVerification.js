bd671c664597b31ab974956f2602f1c2cd57ad2d
benmesswass <benmesswass@gmail.com>
2023-01-25 14:13:59 +0100

    add expected list on json file

--- cypress/e2e/TC003_homePageVerification.js
+++ cypress/e2e/TC003_homePageVerification.js
@@ -15,21 +15,14 @@ describe('homePageVerification', () => {
   })
 
   it('products verification', () => {
-    cy.get('.d-flex.flex-column.col-6.col-sm-6.col-md-3.ng-star-inserted').should('have.length', 7)
-    const expectedTextList = ['NIKE PEGASUS TRAIL 3', 
-                              'ADIDAS ULTRABOOST 21',
-                              'NEW BALANCE RC ELITE V2',
-                              'ADIDAS ULTRABOOST 21',
-                              'NIKE JOYRIDE RUN FLYKNIT',
-                              'NIKE JOYRIDE RUN FLYKNIT',
-                              'NEW BALANCE TC V1'];
-    cy.get('.d-flex.flex-column.col-6.col-sm-6.col-md-3.ng-star-inserted').each(($el, index) => {
-      const actualText = $el.text();
-      const expectedText = expectedTextList[index];
-      expect(actualText).to.contain(expectedText);
-    });
-
-
+    cy.fixture('example.json').then((example) => {
+      const myList = example.expectedTextList;
+      cy.get('.d-flex.flex-column.col-6.col-sm-6.col-md-3.ng-star-inserted').each(($el, index) => {
+        const actualText = $el.text();
+        const expectedText = myList[index];
+        expect(actualText).to.contain(expectedText);
+      });
+    });    
   })
 
 
