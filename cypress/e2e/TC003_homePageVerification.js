11bb4508dd063a33cf44ca20123466f2a23836e6
benmesswass <benmesswass@gmail.com>
2023-01-27 15:14:01 +0100

    add price verification

--- cypress/e2e/TC003_homePageVerification.js
+++ cypress/e2e/TC003_homePageVerification.js
@@ -10,13 +10,13 @@ describe('homePageVerification', () => {
     cy.contains('Adidas').should('be.visible')
     cy.contains('Nike').should('be.visible')
     cy.get(".mat-mdc-tab-header-pagination-chevron").should('have.length', 2)
-    cy.get(".mat-mdc-tab-header-pagination-chevron").eq(1).click()
+    cy.get(".mat-mdc-tab-header-pagination-chevron").eq(1).click({force: true })
     cy.contains('New Balance').should('be.visible')
   })
 
   it('products verification', () => {
     cy.fixture('example.json').then((example) => {
-      const myList = example.expectedTextList;
+      const myList = example.expectedProductsTextList;
       cy.get('.d-flex.flex-column.col-6.col-sm-6.col-md-3.ng-star-inserted').each(($el, index) => {
         const actualText = $el.text();
         const expectedText = myList[index];
@@ -24,4 +24,16 @@ describe('homePageVerification', () => {
       });
     });    
   })
+
+  it('prices verification', () => {
+    cy.fixture('example.json').then((example) => {
+      const myList = example.expectedPricesList;
+      cy.get('.mat-mdc-card-actions.mdc-card__actions.d-flex.flex-row').each(($el, index) => {
+        const actualText = $el.text();
+        const expectedText = myList[index];
+        expect(actualText).to.eq(expectedText);
+      });
+    });    
+  })
+
 })
