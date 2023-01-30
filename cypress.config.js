167b3a57cef10037d96bccd7d6cf454c79ee5d58
benmesswass <benmesswass@gmail.com>
2023-01-30 16:45:30 +0100

    add env variable

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require("cypress");
+//const { fx } = require("cypress/types/jquery");
+const fs = require('fs-extra')
+const path = require('path')
 const cucumber = require('cypress-cucumber-preprocessor').default
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress/config', `${file}.json`);
+
+  if(!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file found.");
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile);
+}
 
 module.exports = defineConfig({
   projectId: "d9fkzi",
@@ -7,9 +21,20 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('file:preprocessor', cucumber())
+
+      const file = config.env.configFile || ''
+
+      return getConfigurationByFile(file)
+    },
+    retries: {
+      runMode: 0,
+      openMode: 1
+    },
+    env: {
+      homePage: "https://clickandcollect-324914.firebaseapp.com/home",
+      first_name: "Wass"
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
-    baseUrl: "https://clickandcollect-324914.firebaseapp.com/home",
     screenshotOnRunFailure: true,
     trashAssetsBeforeRuns: true
   },
